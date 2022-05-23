03c4717c7355defc6ac7f4064b1d61990d0a14a3
jordanianpizza <jordangraf42@gmail.com>
2022-05-23 17:22:19 +0930

    Add class to check word against dictionary to be implemented

--- src/src/WordChecker.java
+++ src/src/WordChecker.java
@@ -1,2 +1,6 @@
 public class WordChecker {
+
+    public void checkWord(String word, int[] arr) {
+        //TODO implement a function here to check the word and array inputted by the user and thin the dictionary
+    }
 }
