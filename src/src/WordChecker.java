a90d0a0e270a6ab4427362ebae1fdd86d7f84d48
jordanianpizza <jordangraf42@gmail.com>
2022-05-24 12:48:07 +0930

    Remove wordChecker as functionality implemented in dictionary

--- src/src/WordChecker.java
+++
@@ -1,6 +0,0 @@
-public class WordChecker {
-
-    public void checkWord(String word, int[] arr) {
-        //TODO implement a function here to check the word and array inputted by the user and thin the dictionary
-    }
-}
