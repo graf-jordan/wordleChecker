be96a389a7d3a9ae71e951a7956a3dfdc01a6f03
jordanianpizza <jordangraf42@gmail.com>
2022-05-24 13:21:38 +0930

    Refine and abstract user interface - program now functional

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 public class Main {
+
     public static void main(String[] args) throws FileNotFoundException {
-        makeBoxHor();
-        askUserForInput();
-        makeBoxHor();
-        WordChecker wc = new WordChecker();
         Dictionary words = new Dictionary();
-        String toCheck = getUserInput();
-        makeBoxHor();
-        int[] temp = getWordInPosition(toCheck);
-        makeBoxHor();
-        words.parseWord(toCheck, temp);
-        makeBoxHor();
-        words.printValidWords();
+        askUserForInput();
+        while (true) {
+            String toCheck = getUserInput();
+            boolean toEnd = isCorrect();
+            if (toEnd) {
+                break;
+            }
+            int[] temp = getWordInPosition(toCheck);
+            words.parseWord(toCheck, temp);
+            showUserWords(toCheck, words);
+        }
     }
+    //the following functions are used for interface abstraction, except for the very last one
+    //the last one is simply for ensuring program does not crash when user enters string when asked for int
     public static void makeBoxHor() {
         String boxTop = "==========================================================================================";
         System.out.printf(boxTop + "\n");
     }
     public static void askUserForInput(){
+        makeBoxHor();
         String userAsk = "Please enter a five letter word";
         userAsk += "\n";
         userAsk += "This program will try and guide you to a correct Wordle guess!";
         System.out.println(userAsk);
+        makeBoxHor();
     }
     public static String getUserInput(){
         Scanner sc = new Scanner(System.in);
@@ -34,7 +39,7 @@ public class Main {
         }
         return word;
     }
-    public static int[] getWordInPosition(String word) {
+    public static int[] getWordInPosition(String word) { //function asks user for the Wordle values of each letter
         Scanner sc = new Scanner(System.in);
         System.out.println("What was the status of each letter? 1 = grey, 2 = yellow, 3 = green");
         int posCheck = 0;
@@ -50,6 +55,35 @@ public class Main {
             posCheck++;
         }
         return wordValues;
+    }
+    public static void showUserWords(String word, Dictionary words) { //function to print how many words they could guess from
+        makeBoxHor();
+        System.out.println("Using your inputted word of " + word + " you can select any of these words");
+        makeBoxHor();
+        words.printValidWords();
+        System.out.println();
+        makeBoxHor();
+    }
+    public static boolean isCorrect() { //ask user if the word they guessed was correct
+        Scanner sc = new Scanner(System.in);
+        makeBoxHor();
+        String correct = "0";
+        while(!correct.equals("y") && !correct.equals("n")) {
+            System.out.println("Was your guess correct? y/n");
+            correct = sc.nextLine();
+            if (correct.equals("y")) {
+                System.out.println("Congrats! good job on your Wordle!");
+                makeBoxHor();
+                return true;
+            } else if (correct.equals("n")) {
+                System.out.println("Alright! Let's keep trying!");
+                makeBoxHor();
+                return false;
+            }
+            System.out.println("Please enter y or n");
+            correct = sc.nextLine();
+        }
+        return false;
     }
     public static int getCheckedInt() { //small method to call upon to return an integer that only accepts int
         Scanner sc = new Scanner(System.in);
